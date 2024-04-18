b00ef9c5b1256ed3135ed9bb978382a0949d3daa
Randy <randy@kmote.mx>
2024-04-18 16:34:30 -0600

    init

---
+++ script.js
@@ -0,0 +1,12 @@
+﻿// Ocultar el contenido hasta que la página se cargue completamente
+document.addEventListener("DOMContentLoaded", function () {
+    document.getElementById("content").style.display = "none";
+});
+
+// Mostrar el contenido después de un tiempo determinado (2 segundos en este caso)
+window.onload = function () {
+    setTimeout(function () {
+        document.getElementById("content").style.display = "block";
+        document.getElementById("loader").style.display = "none";
+    }, 2000);
+};
